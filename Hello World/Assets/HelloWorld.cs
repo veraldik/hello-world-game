8541edb0f4fbd744ef537ecfe418017568a8ece2
Kimberly Veraldi <kimveraldi@gmail.com>
2019-07-22 16:26:11 -0400

    uploading source files

---
+++ Hello World/Assets/HelloWorld.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour {
+
+	public string myName = "Jordan";
+	public GameObject cubePrefab;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GameObject go =	Instantiate<GameObject> (cubePrefab);
+		go.transform.position = Random.insideUnitSphere;
+		go.transform.rotation = Random.rotation;
+
+		Renderer rend = go.GetComponent<Renderer> ();
+		Material mat = rend.material;
+		mat.color = new Color (Random.value, Random.value, Random.value);
+	}
+}
